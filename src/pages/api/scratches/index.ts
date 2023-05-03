ea918345ea6741a67423d2d552b41a6121c02a7a
antonpalermo <ANPBusinessmail@gmail.com>
2023-05-03 22:21:21 +0800

    added prisma client

---
+++ src/pages/api/scratches/index.ts
@@ -0,0 +1,32 @@
+import { Prisma } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "@gizmo/utils/prisma";
+import getAuthServerSession from "@gizmo/utils/auth/getAuthServerSession";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getAuthServerSession(req, res);
+
+  try {
+    switch (req.method) {
+      case "POST":
+        const { header } = req.body;
+        const createdScratch = await prisma.scratches.create({
+          data: { header, owner: session?.user?.email as string }
+        });
+        return res.status(201).json(createdScratch);
+
+      default:
+        return res.status(405).end();
+    }
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // TODO: Log error here.
+      console.log(e);
+    }
+    res.status(500).end();
+  }
+}
