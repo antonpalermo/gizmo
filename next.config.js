cd4d49e929410f860e7d4328bae3b63f5c1edc82
antonpalermo <ANPBusinessmail@gmail.com>
2023-05-16 23:19:02 +0800

    added redirects

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["lh3.googleusercontent.com"]
-  }
+  },
+  redirects: async () => [
+    { source: "/", destination: "/dashboard", permanent: false }
+  ]
 };
 
 module.exports = nextConfig;
