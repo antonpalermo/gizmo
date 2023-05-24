a27019685086dac7be57543a6beef38236a0d08a
antonpalermo <ANPBusinessmail@gmail.com>
2023-05-24 23:03:49 +0800

    blog post

--- src/app/(dashboard)/dashboard/drafts/page.tsx
+++ src/app/(dashboard)/dashboard/drafts/page.tsx
@@ -1,3 +1,13 @@
+import { prisma } from "@gizmo/utils/prisma";
+
 export default async function DraftsPage() {
-  return <h1>Drafts Page</h1>;
+  const blogs = await prisma.blogs.findMany();
+
+  return (
+    <div>
+      {blogs.map(blog => (
+        <h1 key={blog.id}>{blog.title}</h1>
+      ))}
+    </div>
+  );
 }
