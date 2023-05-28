0997704f4944937792b55857f9b1ad5d82aeddfa
anton palermo <anpbusinessmail@gmail.com>
2023-05-28 23:15:17 +0800

    refac: update schema

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -23,17 +23,13 @@ export default async function DashboardLayout({
   return (
     <div className="flex min-h-screen flex-col space-y-5">
       <header className="sticky top-0 z-40 border-b border-gray-200 bg-slate-50">
-        <div className="container mx-auto flex items-center justify-between py-4">
-          <div className="inline-flex items-center space-x-3">
-            <h1>sample</h1>
-            <Link
-              href={"/dashboard/blogs"}
-              className="rounded px-3 py-2 font-medium text-gray-600 hover:bg-gray-300 hover:text-gray-900"
-            >
-              Blogs
-            </Link>
+        <div className="container mx-auto flex w-full items-center justify-end py-4">
+          <div className="inline-flex items-center">
+            <div className="mr-2">
+              <button className="rounded hover:bg-slate-500 px-3 py-2">Create</button>
+            </div>
+            <UserNavMenu user={user} />
           </div>
-          <UserNavMenu user={user} />
         </div>
       </header>
       <div className="container mx-auto grid gap-10 md:grid-cols-[230px_1fr]">
