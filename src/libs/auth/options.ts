1e749251eca331ece23357dab86949aee330fef7
anton palermo <anpbusinessmail@gmail.com>
2023-06-15 20:59:36 +0800

    fix: broken avatar

--- src/libs/auth/options.ts
+++ src/libs/auth/options.ts
@@ -23,23 +23,23 @@ const options: NextAuthOptions = {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     })
-  ],
-  callbacks: {
-    async session({ token, session }) {
-      if (session.user) {
-        session.user = token;
-      }
+  ]
+  // callbacks: {
+  //   async session({ token, session }) {
+  //     if (session.user) {
+  //       session.user =;
+  //     }
 
-      return session;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-        token.role = user.role;
-      }
-      return token;
-    }
-  }
+  //     return session;
+  //   },
+  //   async jwt({ token, user }) {
+  //     if (user) {
+  //       token.id = user.id;
+  //       token.role = user.role;
+  //     }
+  //     return token;
+  //   }
+  // }
 };
 
 export default options;
